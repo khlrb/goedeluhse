7a995a9c7378964785b09383cb4feaf613799142
kohlrabi <kohlrabi@eosphorit>
2017-12-28 13:00:59 +0100

    solver

--- src/Clause.hs
+++ src/Clause.hs
@@ -1,8 +1,9 @@
 module Clause where
 
 import Control.Monad
-import Data.Set
-
+import Data.Set (fromList, delete, union)
+import Data.Foldable (find)
+import Data.Maybe (catMaybes)
 import Expression
 
 data Lit = Plain Int
@@ -23,8 +24,16 @@ toClause :: E -> Maybe Clause
 toClause (a `Or` b) = liftM2 union (toClause a) (toClause b)
 toClause e          = singleton <$> toLit e
 
-fromExpr :: E -> Maybe (Set Clause)
-fromExpr x = (f . cnf) x
-  where f (a `And` b) = liftM2 (union) (f a) (f b)
-        f e           = singleton <$> toClause e
+fromCNF :: E -> Maybe (Set Clause)
+fromCNF (a `And` b) = liftM2 union (fromCNF a) (fromCNF b)
+fromCNF e           = singleton <$> toClause e
 
+resolveClause :: Clause -> Clause -> Maybe Clause
+resolveClause x y = (\a -> union (delete a x) (delete (negateLit a) y)) <$> find (\l -> (negateLit l) `member` y) x
+
+resolveClauseSet :: Set Clause -> Set Clause
+resolveClauseSet clauses = clauses `union` fromList [ a | Just a <- [ resolveClause l r | l <- toList $ clauses, r <- toList $ clauses, l /= r]]
+
+resolve :: Set Clause -> Set Clause
+resolve clauses = if (size resolution) /= (size clauses) then resolve resolution else resolution
+  where resolution = resolveClauseSet clauses
